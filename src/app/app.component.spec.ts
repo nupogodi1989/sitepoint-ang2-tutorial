f450ea850d0a2387d1cdd629ad4c60355aa5b06a
thermos <coffeestain@protonmail.com>
2018-11-19 22:44:28 -0700

    replace old test with 'should have a newTodo todo'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        FormsModule,
         RouterTestingModule
       ],
       declarations: [
@@ -20,10 +23,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-app'`, () => {
+  it(`should have a newTodo todo`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-app');
+    expect(app.newTodo instanceof Todo).toBeTruthy();
   });
 
   it('should render title in a h1 tag', () => {
