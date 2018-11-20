1b09ba5ed96af98d799fed30410fbf8540197adb
thermos <coffeestain@protonmail.com>
2018-11-19 22:10:05 -0700

    implement logic for view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'todo-app';
+
+  newTodo: Todo = new Todo();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  // Service is now available as this.todoDataService
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  getTodos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
